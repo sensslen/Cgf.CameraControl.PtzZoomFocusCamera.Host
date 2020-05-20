72b2353cc0dbfa2e06e6cddb954c22dad1fc7e36
Simon Ensslen <simon.ensslen@griesser.ch>
2020-05-20 19:26:41 +0200

    Initial compiling version that could theoretically work

--- Source/CameraControl/CameraControl.ino
+++ Source/CameraControl/CameraControl.ino
@@ -1,54 +1,118 @@
-#include <TimerOne.h>
+#include <LibLanc.h>
+#include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h>
 
-// This example uses the timer interrupt to blink an LED
-// and also demonstrates how to share a variable between
-// the interrupt and the main program.
+// Listen to the default port 5555, the Yún webserver
+// will forward there all the HTTP requests you send
+BridgeServer server;
 
-
-const int led = LED_BUILTIN;  // the pin with a LED
+const int LancOut = 1;  // the pin which is connected to Lanc Output
+const int LancIn = 2;  // the pin which is connected to Lanc Input
 
 void setup(void)
 {
-  pinMode(led, OUTPUT);
-  Timer1.initialize(150000);
-  Timer1.attachInterrupt(blinkLED); // blinkLED to run every 0.15 seconds
-  Serial.begin(9600);
-}
+  // Bridge takes about two seconds to start up
+  // it can be helpful to use the on-board LED
+  // as an indicator for when it has initialized
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Bridge.begin();
+  digitalWrite(LED_BUILTIN, HIGH);
 
+  Lanc::InstanceGet()->Setup(LancIn, LancOut);
 
-// The interrupt will blink the LED, and keep
-// track of how many times it has blinked.
-int ledState = LOW;
-volatile unsigned long blinkCount = 0; // use volatile for shared variables
-
-void blinkLED(void)
-{
-  if (ledState == LOW) {
-    ledState = HIGH;
-    blinkCount = blinkCount + 1;  // increase when LED turns on
-  } else {
-    ledState = LOW;
-  }
-  digitalWrite(led, ledState);
+  // Listen for incoming connection only from localhost
+  // (no one from the external network could connect)
+  server.listenOnLocalhost();
+  server.begin();
 }
 
 
 // The main program will print the blink count
 // to the Arduino Serial Monitor
 void loop(void)
-{
-  unsigned long blinkCopy;  // holds a copy of the blinkCount
+{ // Get clients coming from server
+  BridgeClient client = server.accept();
 
-  // to read a variable which the interrupt code writes, we
-  // must temporarily disable interrupts, to be sure it will
-  // not change while we are reading.  To minimize the time
-  // with interrupts off, just quickly make a copy, and then
-  // use the copy while allowing the interrupt to keep working.
-  noInterrupts();
-  blinkCopy = blinkCount;
-  interrupts();
+  // There is a new client?
+  if (client) {
+    // Process request
+    process(client);
 
-  Serial.print("blinkCount = ");
-  Serial.println(blinkCopy);
-  delay(100);
+    // Close connection and free resources.
+    client.stop();
+  }
+
+  //Poll as Quickly as possible
+  //delay(50); // Poll every 50ms
+}
+
+void process(BridgeClient client) {
+  // read the command
+  String command = client.readStringUntil('/');
+
+  if (command == "zoom") {
+    zoomCommand(client);
+  }
+
+  if (command == "focus") {
+    focusCommand(client);
+  }
+
+  if (command == "autoFocus") {
+    autoFocusCommand(client);
+  }
+}
+
+void zoomCommand(BridgeClient client) {
+  int value;
+
+  // Read zoom value
+  value = client.parseInt();
+
+  bool successful = false;
+  if (value >= -8 && value <= 8) {
+    successful = Lanc::InstanceGet()->Zoom((int8_t) value);
+  }
+
+  // Send feedback to client
+  client.print(F("Zoom Value"));
+  client.print(value);
+  client.print(F(" sent successfully:"));
+  client.println(successful);
+
+  // Update datastore key with the current pin value
+  String key = "Zoom";
+  Bridge.put(key, String(value));
+}
+
+void focusCommand(BridgeClient client) {
+  int value;
+
+  // Read focus value
+  value = client.parseInt();
+
+  bool successful = false;
+  if (value >= 0 && value <= 1) {
+    successful = Lanc::InstanceGet()->Zoom((bool) value);
+  }
+
+  // Send feedback to client
+  client.print(F("Focus Value"));
+  client.print(value);
+  client.print(F(" sent successfully:"));
+  client.println(successful);
+
+  // Update datastore key with the current pin value
+  String key = "Focus";
+  Bridge.put(key, String(value));
+}
+
+void autoFocusCommand(BridgeClient client) {
+  bool successful = Lanc::InstanceGet()->AutoFocus();
+
+  // Send feedback to client
+  client.print(F("Autofocus Command sent successfully:"));
+  client.println(successful);
 }
