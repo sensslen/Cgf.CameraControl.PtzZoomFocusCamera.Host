f9f0f30715db86ab74ff688f613a2b98b890e132
Simon Ensslen <simon.ensslen@griesser.ch>
2020-07-01 23:09:50 +0200

    Use new interface that allows much faster decoding of message and also request next command before executing lanc command so that the response should be available when the lanc loop finishes. Also do Serial communication as asynchronously as possible.

--- Source/CameraControl/CameraControl.ino
+++ Source/CameraControl/CameraControl.ino
@@ -5,6 +5,17 @@ Lanc lanc(11, 10);
 #define TILT_UP_PIN  9
 #define TILT_DOWN_PIN  6
 
+typedef void (*tReceptionState)(uint8_t revieced);
+
+void Reception_WaitForStart(uint8_t received);
+void Reception_Receiving(uint8_t received);
+void Reception_ReceptionComplete(uint8_t received);
+
+uint8_t receiveString[6];
+uint8_t receivePosition;
+tReceptionState receptionState;
+int lastCommandRequest;
+
 void setup(void)
 {
   Serial.begin(500000);
@@ -16,6 +27,9 @@ void setup(void)
 
   lanc.begin();
 
+  receptionState = Reception_WaitForStart;
+  lastCommandRequest = 0;
+
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB
   }
@@ -24,10 +38,13 @@ void setup(void)
 bool aliveState = false;
 void loop(void)
 {
-  digitalWrite(LED_BUILTIN, aliveState);
-  aliveState = !aliveState;
   getCurrentCommand();
   lanc.loop();
+}
+
+void requestCommand() {
+  lastCommandRequest = millis();
+  Serial.println("v1");
 }
 
 /**
@@ -41,43 +58,41 @@ void loop(void)
    @note Please note that focus commands override zoom commands
 */
 void getCurrentCommand() {
-  Serial.println("next");
+  // read things
+  while (Serial.available() > 0) {
+    receptionState((uint8_t)Serial.read());
+  }
 
-  auto read = Serial.readStringUntil('\n');
-  int startCharacter = read.indexOf('@');
-  if (startCharacter >= 0) {
-    auto commands = read.substring(startCharacter + 1);
-    processPan(getStringPart(commands, ','));
-    processTilt(getStringPart(commands, ','));
-    processZoom(getStringPart(commands, ','));
-    processFocus(getStringPart(commands, ','));
+  // process when receptionComplete
+  if (receptionState == Reception_ReceptionComplete) {
+    processPan();
+    processTilt();
+    processZoom();
+    processFocus();
+    receptionState = Reception_WaitForStart;
+    requestCommand();
+  }
+
+  // get next command if last reception is too long waited for
+  if ((millis() - lastCommandRequest) > 1000) {
+    requestCommand();
   }
 }
 
-/**
-  Return the first part of the string by searching for the separator.
-  The string given with @p data is modified so that it just contains the remaining string
-*/
-String getStringPart(String &data, char separator)
-{
-  int strIndex[] = { 0, -1 };
-  int maxIndex = data.length() - 1;
-
-  for (int i = 0; i <= maxIndex; i++) {
-    if (data.charAt(i) == separator) {
-      auto retval = data.substring(0, i);
-      data = data.substring(i + 1);
-      return retval;
-    }
-  }
-  auto retval = data;
-  data = "";
-  return retval;
+int decodePanTilt(uint8_t * data) {
+  bool negative = *data & (1 << 4);
+  int retval = ((data[0] % 16) << 5) + data[1];
+  return (negative) ? -retval : retval;
 }
 
-void processPan(String stringValue) {
-  // conversion will return zero if no valid integer
-  int value = stringValue.toInt();
+int decodeZoomFocus(uint8_t * data) {
+  bool negative = (data[0] & (1 << 4)) > 0;
+  int retval = data[0] % 16;
+  return (negative) ? -retval : retval;
+}
+
+void processPan() {
+  int value = decodePanTilt(&receiveString[0]);
 
   if (value < 0) {
     analogWrite(PAN_RIGHT_PIN, 0);
@@ -88,9 +103,8 @@ void processPan(String stringValue) {
   }
 }
 
-void processTilt(String stringValue) {
-  // conversion will return zero if no valid integer
-  int value = stringValue.toInt();
+void processTilt() {
+  int value = decodePanTilt(&receiveString[2]);
 
   if (value < 0) {
     analogWrite(TILT_UP_PIN, 0);
@@ -101,18 +115,16 @@ void processTilt(String stringValue) {
   }
 }
 
-void processZoom(String stringValue) {
-  // conversion will return zero if no valid integer
-  int value = stringValue.toInt();
+void processZoom() {
+  int value = decodeZoomFocus(&receiveString[4]);
 
   // A value of zero will already clear the command
   // therefore no need here to clear manually
   lanc.Zoom(value);
 }
 
-void processFocus(String stringValue) {
-  // conversion will return zero if no valid integer
-  int value = stringValue.toInt();
+void processFocus() {
+  int value = decodeZoomFocus(&receiveString[5]);
   switch (value) {
     case 1:
       lanc.Focus(false);
@@ -123,8 +135,26 @@ void processFocus(String stringValue) {
     default:
       // Don't clear command. This command unfortunately
       // overwrites the Zoom command since it is processed later.
-      // (unfortunately the lanc protocol does not allow simultanous operation 
+      // (unfortunately the lanc protocol does not allow simultanous operation
       // of both commands)
       break;
   }
+}
+
+void Reception_WaitForStart(uint8_t received) {
+  if (received == '?') {
+    receptionState = Reception_Receiving;
+    receivePosition = 0;
+  }
+}
+void Reception_Receiving(uint8_t received) {
+  if (received == '\n') {
+    receptionState = Reception_ReceptionComplete;
+  } else if (receivePosition < (sizeof(receiveString)/sizeof(receiveString[0]))) {
+    receiveString[receivePosition] = received;
+    receivePosition ++;
+  }
+}
+void Reception_ReceptionComplete(uint8_t received) {
+  // do nothing intentionally
 }
