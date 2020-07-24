3c7f303c4121cf83bf65402393e90cb23d516e61
Simon Ensslen <simon.ensslen@griesser.ch>
2020-07-24 20:06:45 +0200

    âdd missing addion to previous checkin

--- CGF.CameraControl.Provider/Controllers/PanTiltZoomController.cs
+++ CGF.CameraControl.Provider/Controllers/PanTiltZoomController.cs
@@ -62,25 +62,12 @@ namespace CGF.CameraControl.Provider.Controllers
         [Route("firmware")]
         public async Task<IActionResult> OnPostUploadAsync(IFormFile file)
         {
-            long size = file.Length;
-
-            foreach (var formFile in files)
+            using (var stream = file.OpenReadStream())
             {
-                if (formFile.Length > 0)
-                {
-                    var filePath = Path.GetTempFileName();
-
-                    using (var stream = System.IO.File.Create(filePath))
-                    {
-                        await formFile.CopyToAsync(stream);
-                    }
-                }
+                await _communicator.UploadFirmware(stream);
             }
 
-            // Process uploaded files
-            // Don't rely on or trust the FileName property without validation.
-
-            return Ok(new { count = files.Count, size });
+            return Ok(new { count = 1, file.Length });
         }
     }
 }
