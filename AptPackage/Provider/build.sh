b8804b3747ccbe2f05336ad2b252754efb2fc849
Simon Ensslen <simon.ensslen@griesser.ch>
2020-07-24 20:11:46 +0200

    correct build script

--- AptPackage/Provider/build.sh
+++ AptPackage/Provider/build.sh
@@ -1,4 +1,4 @@
 
 input=$1
 
-dotnet publish $input -r linux-arm -c Release /p:PublishSingleFile=true --self-contained /p:PublishTrimmed=true
+dotnet publish $input -r linux-arm -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true
