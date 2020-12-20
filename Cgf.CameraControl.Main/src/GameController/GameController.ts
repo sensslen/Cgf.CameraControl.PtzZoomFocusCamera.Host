4b116aee49cbba7b30e30fa03e226da2a3a3f7a3
Chrischona Frauenfeld <info@chrischona-frauenfeld.ch>
2020-12-20 09:41:37 +0100

    Send stop of zoom/focus and pan/tilt before issuing a connection change

--- Cgf.CameraControl.Main/src/GameController/GameController.ts
+++ Cgf.CameraControl.Main/src/GameController/GameController.ts
@@ -135,6 +135,7 @@ export class GameController {
         }
 
         if (next) {
+            this._currentCameraConnection?.connection.setState(new State());
             this.atem.changePreview(this._atemMixEffectBlock, next);
         }
     }
